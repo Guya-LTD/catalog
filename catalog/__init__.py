b8930e15d16e07be80c0422aa1d04db3f67b5a86
semantic-release <semantic-release>
2020-09-06 17:26:24 +0000

    0.2.0
    Automatically generated by python-semantic-release

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -54,7 +54,7 @@ from .handlers import register_handler
 from catalog.controller.v1 import inventory_controller
 
 # const vars
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 __email__ = 'simonbelete@gmail.com'
 __title__ = 'Catalog Service'
 __author__ = 'Simon Belete'
