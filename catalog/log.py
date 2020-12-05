615a88c8075f67f1770208d9881c3f38d9ffcd9f
Simonbelete <simonbelete@gmail.com>
2020-12-05 12:12:50 +0300

    Logging handlers

--- catalog/log.py
+++ catalog/log.py
@@ -22,9 +22,9 @@ Project
     * Name: 
         - Guya E-commerce & Guya Express
     * Sub Project Name:
-        - Catalog Service
+        - Branch Service
     * Description
-        - Catlog Catalog Service
+        - Branch location and details service
 """
 
 
@@ -38,7 +38,7 @@ import logstash
 
 # const vars
 __extra__ = {
-    'app_name': 'Catalog Service',      
+    'app_name': 'Branch Service',      
     'environment': os.getenv('ENV'),  
     'container_host': os.getenv('HOST'),
     'port': os.getenv('PORT'),
@@ -56,14 +56,17 @@ logger = logging.getLogger(__name__)
 
 # logger handler
 logger.addHandler(
-    logstash.LogstashHandler(
+    logstash.TCPLogstashHandler(
+        #"logstash-logstash.guya-ltd-elk.svc.cluster.local",
+        #5400, 
         os.getenv('LOGGING_HOST'),
         os.getenv('LOGGING_PORT'), 
-        version=1)
+        version=1
     )
-
+)
 
 def log_exception(error, extra) -> None:
+    myextra = __extra__
+    myextra.update(extra)
     # recommanded for production or eithre the logging servier is running
-    # logger.exception(str(error), extra = __extra__.update(extra))
-    pass
+    logger.exception(str(error), extra = myextra)
